6a690098511bfca503d45ce98dd0fb328fd5732f
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-04 11:15:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheeses.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
+    cheese.include?(maybe_cheese)
   end
 end
