a5b17edd0541480e6b2cacf4fbc8a039673b4628
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-04 10:52:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,16 @@ def roll_call_dwarves (dwarves)
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+  def summon_captain_planet(array)
+  new_array = []
+  i = 0
+  exclaim = array.join("! ")
+  exclaim.capitalize()
+  split = exclaim.split(" ")
+  cap = split.map(&:capitalize)
+  cap
+end
 end
 
 def long_planeteer_calls# code an argument here
