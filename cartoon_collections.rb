6aced29194a88574d27c3b155dc6d6214e26effa
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-04 11:08:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 
 def long_planeteer_calls
-  if array.any? 
+  planeteer_calls.any? { |call| call.length > 4 }
 
 def find_the_cheese# code an argument here
   # the array below is here to help
